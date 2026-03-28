84000a04f02555fe68c01118ad64b7080cfdd0aa
Adit122022 <adity202212@gmail.com>
2026-03-29 02:33:18 +0530

    all good now

--- React_Native/Recurrely/app/index.tsx
+++ React_Native/Recurrely/app/index.tsx
@@ -6,6 +6,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
+
       </Text>
     </View>
   );
