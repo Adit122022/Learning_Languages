52f42d26ba56835ea636e13a01250583be141267
Adit122022 <adity202212@gmail.com>
2026-04-03 23:36:18 +0530

    refactor: update app layout and enhance link styling; clean up .easignore

--- React_Native/Recurrely/app/index.tsx
+++ React_Native/Recurrely/app/index.tsx
@@ -4,11 +4,16 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-foreground">
+    <View className="flex-1 items-center gap-5 justify-center bg-black">
       <Text className="text-xl font-bold text-success font-mono">
         Welcome to Nativewind!
       </Text>
-      <Link className="px-2 py-4 bg-foreground  text-muted" href="/Onboarding">OnBoarding</Link>
+      {/* Links */}
+      <View className="flex-row gap-5">
+        <Link className="px-4 py-2 bg-white  rounded-xl font-bold capitalize text-muted-foreground" href="/Onboarding">OnBoarding</Link>
+        <Link className="px-4 py-2 bg-primary  rounded-xl font-bold capitalize text-muted" href="/(auth)/SignIn">Login</Link>
+        <Link className="px-4 py-2 bg-white  rounded-xl font-bold capitalize text-muted-foreground" href="/(auth)/SignUp">Register</Link>
+      </View>
     </View>
   );
 }
