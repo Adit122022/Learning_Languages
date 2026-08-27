65e1515878b59f52c47e5b84b2789dbad9708a5b
Adit122022 <adity202212@gmail.com>
2026-08-27 11:53:25 +0530

    feat: initialize project structure with Expo Router, NativeWind configuration, and auth layout scaffolding

--- React_Native/Recurrely/app/(auth)/SignIn.tsx
+++ React_Native/Recurrely/app/(auth)/SignIn.tsx
@@ -5,10 +5,12 @@ import { Text, View } from 'react-native'
 
 const SignIn = () => {
   return (
-    <View className='bg-background min-h-screen min-w-screen'>
+    <View className='bg-background min-h-screen min-w-screen '>
+      <View className='flex items-center justify-center'>
 
-      <Text className='text-center font-mono text-2xl text-foreground font-bold'> Sign In</Text>
-      <Button className='px-2 py-4 flex justify-center items-center bg-foreground  text-muted' onPress={() => router.push("/(auth)/SignUp")}>SignUp</Button>
+        <Text className='text-center font-mono text-2xl text-foreground font-bold'> Sign In</Text>
+        <Button className='px-2 py-4 flex justify-center items-center bg-foreground  text-muted' onPress={() => router.push("/(auth)/SignUp")}>SignUp</Button>
+      </View>
     </View>
   )
 }
--- React_Native/Recurrely/app/_layout.tsx
+++ React_Native/Recurrely/app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
 import "@/global.css";
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return <Stack screenOptions={{
+    headerShown: false,
+    animation: "fade_from_bottom",
+    contentStyle: {
+      backgroundColor: "#141414",
+    },
+    
+  }} />;
 }
